1c516a129dbffcbbad672166f0f88f0f84df417b
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2026-03-09 16:03:30 +0100

    feat: calendar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,6 +4,7 @@ import { Heatmap } from "./components/Heatmap";
 import { orpcUtils } from "./lib/orpc";
 import { Weather } from "./components/Weather/Weather";
 import bg from "./assets/bg.jpg";
+import { Calendar } from "./components/Calendar/Calendar";
 
 function App() {
   const { data, error, isPending } = useQuery(
@@ -30,16 +31,20 @@ function App() {
   return (
     <Box w="100vw" h="100vh" bgSize="cover" bgImage={`url(${bg})`}>
       <Grid
-        maxW="40rem"
+        maxW="60rem"
         mx="auto"
         pt="5rem"
-        templateColumns="repeat(2, 1fr)"
+        templateColumns="repeat(3, 1fr)"
+        templateRows="repeat(3, 1fr)"
         gap="6"
       >
         <GridItem colSpan={2}>
           <Heatmap weeks={20} activities={data ?? []} />
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={1} rowSpan={2}>
+          <Calendar></Calendar>
+        </GridItem>
+        <GridItem colSpan={2}>
           <Weather />
         </GridItem>
       </Grid>
---
+++ ui/src/components/Calendar/Calendar.tsx
@@ -0,0 +1,17 @@
+import type React from "react";
+import { Card } from "../Card";
+import { Box } from "@chakra-ui/react";
+
+export function Calendar(): React.ReactNode {
+  return (
+    <Card>
+      <Box overflow="hidden" borderRadius="2xl">
+        <iframe
+
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&showPrint=0&mode=AGENDA&showTabs=0&showCalendars=0&showTz=0&showTitle=0&showNav=0&showDate=0&src=bWF0aGlldS5zY2hub29yQGdtYWlsLmNvbQ&color=%233f51b5"
+          height="400"
+        ></iframe>
+      </Box>
+    </Card>
+  );
+}
