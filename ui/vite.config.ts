c440d78cb98c863d2f8614a9faad5ea1aa734d36
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2026-03-11 17:55:36 +0100

    fix: vite build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_URL || "/",
   server: {
     proxy: {
-      '/rpc': 'http://localhost:3000',
+      "/rpc": "http://localhost:3000",
     },
   },
-})
+});
